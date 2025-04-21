2f6bdc733c1c049aa245600c46a9548d393ec08b
tomazit <rainbow85612@gmail.com>
2025-04-21 18:18:28 -0300

    varias funcionalidades terminadas. falta mucho CSS todavía y un menu de 'start' y un contador de minutos y segundos funcional

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 const gameButton = document.querySelector('#clickeable-button');
 const gameContainer = document.querySelector('.game-container');
 const stats = document.querySelector('#stats');
-const reset = document.querySelector('#resetPosition')
+const reset = document.querySelector('#resetPosition');
+const contador = document.querySelector('#contador');
+const start = document.querySelector('#start-button');
 
 const containerWidth = gameContainer.clientWidth;
 const containerHeight = gameContainer.clientHeight;
 const buttonWidth = gameButton.offsetWidth;
 const buttonHeight = gameButton.offsetHeight;
-
 const centerX = (containerWidth - buttonWidth) / 2;
 const centerY = (containerHeight - buttonHeight) / 2;
+
+
+let seconds = 0;
+let minutes = 0;
 
 let clickCounter = 0;
 let missClick = 0;
@@ -39,9 +44,14 @@ gameButton.addEventListener('click', (event)=>{
     console.log('Fallaste: ' + missClick);
 
 
-
     clickCounter++;
     stats.innerHTML = `Aciertos: ${clickCounter}`;
+   
+    
+
+    
+
+    
     
 
 });
@@ -50,6 +60,7 @@ window.addEventListener('DOMContentLoaded', () => {
     gameButton.style.left = `${centerX}px`;
     gameButton.style.top = `${centerY}px`;
 });
+
 
 reset.addEventListener('click', ()=>{
     clickCounter = 0;
@@ -65,5 +76,19 @@ reset.addEventListener('click', ()=>{
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+
+
+// function actualizarContador() {
+//     seconds++;
+    
+//     if (seconds === 60) {
+//         minutes++;
+//         seconds = 0;
+//     }
+//     contador.innerHTML = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+//     console.log(seconds);
+// }
 
 
